a9921a5d7a273cd45296adad267d6cded842f2a4
IrsanJam <irsandjamaludin@gmail.com>
2023-11-24 21:43:43 +0700

    memperbaiki bug di problem1

--- Problem1.ts
+++ Problem1.ts
@@ -1,6 +1,6 @@
 const simpleEquations = (a: number, b: number, c: number) => {
-  for (let x = 1; x < a; x++) {
-    for (let y = 1; y < a - x; y++) {
+  for (let x = -1000; x <= 1000; x++) {
+    for (let y = -10000; y <= 1000; y++) {
       let z = a - x - y;
       if (x + y + z === a && x * y * z === b && x ** 2 + y ** 2 + z ** 2 === c) {
         return `${x} ${y} ${z}`;
